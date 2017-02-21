9407cba57b7a9bee0625b556606670ee8474657a
yancheng <bony0002@hotmail.com>
2017-02-21 20:36:55 +0800

    ISE Python API

--- ISEAPI.py
+++ ISEAPI.py
@@ -6,3 +6,5 @@ if resp.status_code != 200:
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 for todo_item in resp.json():
     print('{} {}'.format(todo_item['id'], todo_item['summary']))
+	
+requests.get('https://api.github.com/user', auth=HTTPBasicAuth('bony0002@hotmail.com', 'Yinfeng009'))
